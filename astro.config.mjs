172f848822c283015f5e3639b08aabb3e9c69bfe
Yutang Shi <a0933786266@gmail.com>
2025-06-07 12:55:36 +0800

    Update css template

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://YutangShi.github.io/YutangShi.github.io/', // 您的GitHub用戶名
+  site: 'https://yutangshi.github.io',
+  base: '',
   integrations: [],
   markdown: {
     shikiConfig: {
